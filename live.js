2ec28078d15bd9720a57e457a45452c7757ea981
Daniel Jackson <dajack05@gmail.com>
2022-06-25 15:54:08 -0400

    update

--- live.js
+++ live.js
@@ -1,25 +1,47 @@
 let currentIdx = 0;
 let videoElems = document.getElementsByClassName('cell');
+const cellWidth = videoElems[0].clientWidth;
 const videoHolder = videoElems[0].parentElement;
 
-setInterval(() => {
-    next();
-}, 4000);
+let playing = true;
 
-function next(){
+setInterval(() => {
+    if (playing) {
+        next();
+    }
+}, 10 * 1000);
+
+function move(dir) {
     // Update Video `active` Class
     videoElems[currentIdx].classList.remove('active');
-    currentIdx++
-    if(currentIdx >= videoElems.length){
+    currentIdx += dir;
+    if (currentIdx >= videoElems.length) {
         currentIdx = 0;
+    } else if (currentIdx < 0) {
+        currentIdx = videoElems.length - 1;
     }
     videoElems[currentIdx].classList.add('active');
 
     // Slide
-    const cellWidth = videoElems[currentIdx].getClientRects()[0].width;
-    videoHolder.style.marginLeft = cellWidth*currentIdx+"px";
+    const offset = (cellWidth * (videoElems.length / 2)) - cellWidth / 2;
+    videoHolder.style.marginLeft = (offset - (cellWidth * currentIdx)) + "px";
 }
 
-function fullscreen(){
-    videoHolder.requestFullscreen();
+function fullscreen() {
+    videoHolder.parentElement.requestFullscreen();
 }
+
+function next() {
+    move(1);
+}
+
+function previous() {
+    move(-1);
+}
+
+function toggle(button) {
+    playing = !playing;
+    button.innerHTML = playing ? "Stop" : "Start";
+}
+
+next();
